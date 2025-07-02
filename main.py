527a952fa428938517af3012a6177fa2ba0e0c0c
eujoaodev@gmail.com <eujoaodev@gmail.com>
2025-07-02 19:02:34 -0300

    estudosPython1

---
+++ main.py
@@ -0,0 +1,11 @@
+# Solicita as notas das 4 provas
+p1 = float(input("Digite a nota da P1: "))
+p2 = float(input("Digite a nota da P2: "))
+p3 = float(input("Digite a nota da P3: "))
+p4 = float(input("Digite a nota da P4: "))
+
+# Calcula a média
+media = (p1 + p2 + p3 + p4) / 4
+
+# Exibe a média
+print("A média das notas é:", media)
