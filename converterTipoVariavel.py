6ff01453e1bca459cf233a1cad0122846db430a6
eujoaodev@gmail.com <eujoaodev@gmail.com>
2025-08-06 17:37:50 -0300

    Estudos Python2.0

---
+++ converterTipoVariavel.py
@@ -0,0 +1,4 @@
+##convertendo tipos
+print(float(1.0))
+print(int(1.0))
+print(str(1.0))
---
+++ testes.py
@@ -0,0 +1,2 @@
+nome, idade = input('Digite seu nome e idade:').split(',')
+print(f'O {nome} tem {idade} anos.')
